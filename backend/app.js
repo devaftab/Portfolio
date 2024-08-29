fbf65cbfd2d950e59ce9157d4d38db53b4d76c8c
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-08-29 20:32:01 +0530

    serve frontend in backend

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,13 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import MongoDB_connection from "./DB/MondoDB_conection.js";
 import contactRoute from "./routes/contactRoute.js";
-// import path from "path";
-// import { fileURLToPath } from "url";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 //deployement start
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // app.use(express.static(path.join(__dirname, "/frontend/dist")));
 // app.get("*", function (req, res) {
@@ -32,6 +32,13 @@ app.use(bodyParser.json());
 const PORT = process.env.port || 5000;
 
 app.use("/api", contactRoute);
+
+app.use('/', express.static(path.join(__dirname, '../frontend/dist'), {maxAge: 0}));
+app.get('/*', (req, res) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+  res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
+});
+
 
 app.listen(PORT, () => {
   MongoDB_connection();
