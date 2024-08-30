f11726a8b48ead9fa3eac0802d28b40ee98fccb1
devaftab <web.dev.aftab@gmail.com>
2024-08-30 10:15:25 +0530

    frontend url  update

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -24,7 +24,7 @@ function Contact() {
         }
 
         try {
-            const response = await axios.post("https://my-portfolio-api-nu.vercel.app/api/contact", { name, email, message });
+            const response = await axios.post("https://myportfolio-api-rho.vercel.app/api/contact", { name, email, message });
             setModalMessage(response.data.message);
             setIsModalOpen(true);
             setName('');
